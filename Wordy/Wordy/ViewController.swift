f6cd9d45dce7e8877592e8a570256f7ad87d0b9d
Andreas Gjaerum <andreas@burnttoast.no>
2021-10-05 00:19:49 +0200

    Initial commit

---
+++ Wordy/Wordy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wordy
+//
+//  Created by Andreas Gjaerum on 04/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
